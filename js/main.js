578ef17794062df6a5c5672b66a30e5ba3b01581
Nahiyan47 <36642570+Nahiyan47@users.noreply.github.com>
2025-05-05 00:59:17 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -34,6 +34,7 @@ $(document).ready(function($) {
 		$('.home-slider').owlCarousel({
 	    loop:true,
 	    autoplay: true,
+	    autoplayTimeout: 2000,
 	    margin:0,
 	    animateOut: 'fadeOut',
 	    animateIn: 'fadeIn',
